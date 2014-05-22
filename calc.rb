e648c65557b46da2fdf6dece4d3074f143e99c0a
SherrySeibel <Sherry.Seibel@gmail.com>
2014-05-21 21:08:16 -0400

    Simple calculations, ch. 2

---
+++ calc.rb
@@ -0,0 +1,66 @@
+
+#first exercises in Learn to Program
+puts "Simple equation"
+puts 1 + 2
+puts ""
+
+#Floats
+puts "Mathmatical functions using floats:"
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts""
+
+#Integers
+puts "Mathmatical finctions using integers:"
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+puts ""
+
+#Math in parenthesis
+puts "More complex math:"
+puts 5 * (12 - 8) + -15
+puts 98 + (59872 / (13 * 8)) * -51
+puts ""
+
+#How mamy hours are in a year?
+print"The nuber of hours in a year: "
+one_day = 24
+days_in_year = 365
+answer = one_day * days_in_year
+puts answer
+puts ""
+
+#How many minutes are in a decade? 
+print "The number of minutes in a decade(excluding leap years): "
+minutes = 1
+hours = minutes * 60
+single_day = 24 * hours
+year = 365 * single_day
+one_decade = 10 * year
+puts one_decade
+puts""
+
+#My age in seconds (estimate)
+print"My age in seconds: "
+second = 1
+minute = second * 60
+hour = minute * 60
+day = hour * 24
+year = 365 * day
+my_age = year * 25
+puts my_age
+puts""
+
+#Author's age in seconds
+print "The author's age: "
+sec = 1160000000
+min = sec / 60
+hr = min / 60
+d = hr / 24
+authors_age = d / 365
+puts authors_age
+
