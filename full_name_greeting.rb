60d8ddd9ac7c77a7e024477e04285604d58c2947
SherrySeibel <Sherry.Seibel@gmail.com>
2014-05-21 22:14:41 -0400

    Simple 'puts' and 'gets' program

---
+++ full_name_greeting.rb
@@ -0,0 +1,8 @@
+puts 'Hello! What is your first name?'
+first = gets.chomp
+puts 'What is your middle name?'
+middle = gets.chomp
+puts 'What is your last name?'
+last = gets.chomp
+
+puts 'It\'s nice to meet you, ' + first + ' ' + middle + ' ' + last
