c6326e0f4de4b24d15ad8c8d9e1fd70d4efb053f
SherrySeibel <Sherry.Seibel@gmail.com>
2014-05-22 20:33:10 -0400

    'puts' & 'gets' program with added .length method

--- full_name_greeting.rb
+++ full_name_greeting.rb
@@ -5,4 +5,7 @@ middle = gets.chomp
 puts 'What is your last name?'
 last = gets.chomp
 
-puts 'It\'s nice to meet you, ' + first + ' ' + middle + '' + last
+puts 'It\'s nice to meet you, ' + first + ' ' + middle + ' ' + last
+
+full_name = first.length + middle.length + last.length
+puts "your name has " + full_name.to_s + " characters in it."
