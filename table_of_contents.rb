9f03aebc6da80dca2a8c15f663db29f3b4713425
SherrySeibel <Sherry.Seibel@gmail.com>
2014-05-23 23:09:41 -0400

    Practice with .center .rjust .ljust

---
+++ table_of_contents.rb
@@ -0,0 +1,6 @@
+line_width = 50
+puts("Table of Contents".center(line_width)) 
+puts " "
+puts"Chapter 1: Getting Started".ljust(line_width) + "page 1".rjust(line_width)
+puts"Chapter 2: Numbers".ljust(line_width)+ "page 9".rjust(line_width)
+puts"Chapter 3: Letters".ljust(line_width) + "page 13".rjust(line_width)
