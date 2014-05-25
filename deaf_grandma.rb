a3fbee5a550dcb05b52c5b977ae9f4f122e31822
SherrySeibel <Sherry.Seibel@gmail.com>
2014-05-25 16:54:03 -0400

    While loops with rand & if

---
+++ deaf_grandma.rb
@@ -0,0 +1,14 @@
+#Deaf grandma program
+puts"TELL YOUR GRAMDMA SOMETHING!"
+answer = gets.chomp
+
+while answer != answer.upcase
+	puts "HUH?! SPEAK UP SONNY!!"
+	answer = gets.chomp
+end
+	while answer == answer.upcase && answer != "BYE"
+		puts "NO, NOT SINCE " + (rand(1930..1951).to_s + "!")
+		answer = gets.chomp
+	
+end
+	puts "FINE! LET GRANDMA DIE ALONE!"
