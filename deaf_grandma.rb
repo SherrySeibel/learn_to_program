056916c8325383e8a37366e4333c4a2a04794719
SherrySeibel <Sherry.Seibel@gmail.com>
2014-05-27 18:51:54 -0400

    While loops, if and rand - fixed

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,4 +1,30 @@
-#Deaf grandma program
+# Deaf grandma program -- fixed
+
+
+
+while true
+	puts"TELL YOUR GRAMDMA SOMETHING!"
+	answer = gets.chomp
+	puts ""
+		if answer == "BYE"
+		puts "FINE LET GRAMDMA DIE ALONE!"
+		break
+	end
+	if answer == answer.upcase
+		puts "NO, NOT SINCE " + (rand(1930..1951).to_s + "!")
+		puts ""
+	elsif answer == answer.downcase
+	 	 puts "HUH?! SPEAK UP SONNY!!"
+	 	 puts ""
+	end
+end
+	 	
+
+
+=begin 
+
+There was a glitch in the original code. I have since fixed it and added conditionals
+
 puts"TELL YOUR GRAMDMA SOMETHING!"
 answer = gets.chomp
 
@@ -11,3 +37,4 @@ end
 		answer = gets.chomp	
 	end
 	puts "FINE! LET GRANDMA DIE ALONE!"
+=end
