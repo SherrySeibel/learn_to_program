11a271f693945d4b4f149efd608ab6ade43a45db
SherrySeibel <Sherry.Seibel@gmail.com>
2014-05-28 22:01:22 -0400

    for loop

--- leap_years.rb
+++ leap_years.rb
@@ -9,11 +9,11 @@ ends = gets.chomp.to_i
 
 puts "The following years are leap years: "
 
-year = starts
-until year > ends
+for year in starts..ends
 	
 	if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) 
 	puts "#{year}"
 	end
 	year = year + 1
 end
+ 
