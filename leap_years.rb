a46b5b9f8bb20ed8fc9a86de4757c1d07444a7d7
SherrySeibel <Sherry.Seibel@gmail.com>
2014-05-28 21:46:38 -0400

    More loops

--- leap_years.rb
+++ leap_years.rb
@@ -3,17 +3,17 @@
 print "How many leap years are there between two dates? "
 puts ""
 puts "Enter starting year:"
-starts = gets.chomp
-starts.to_i
+starts = gets.chomp.to_i
 puts "Enter ending year:"
-ends = gets.chomp
-ends.to_i
-ends
+ends = gets.chomp.to_i
 
-loop do |starts|
-	starts += 1
-	next if starts % 4 == 0 && (starts % 100 == 0 && starts % 400 == 0) 
-	puts "#{starts}"
-	break if starts == ends
+puts "The following years are leap years: "
+
+year = starts
+until year > ends
 	
+	if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) 
+	puts "#{year}"
+	end
+	year = year + 1
 end
