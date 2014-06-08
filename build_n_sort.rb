e13b678e2b39800782b2325d50466a6761717c9d
Sherry <Renee@renee.home>
2014-06-08 10:21:15 -0400

    Array practice

---
+++ build_n_sort.rb
@@ -0,0 +1,35 @@
+class Sort
+
+	def initialize
+		@words_to_sort = []
+	end
+
+	def play
+		user_prompt
+		collect_n_store_words
+		sort_n_display_words
+	end
+
+	private
+
+	def user_prompt
+		puts "Enter some words to be sorted: "
+	end
+
+	def collect_n_store_words
+		loop do 
+			words = gets.chomp
+			if words.empty?
+				break
+			else
+			@words_to_sort << words			
+			end
+		end
+	end
+
+	def sort_n_display_words
+		puts @words_to_sort.sort
+	end
+end
+
+Sort.new.play
